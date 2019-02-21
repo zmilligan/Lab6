d96f9f947a77995a60d07aa30f80d176145bd47b
Zandi Milligan <zmilligan@ou.edu>
2019-02-21 13:29:19 -0600

    implemented the class

--- Square.java
+++ Square.java
@@ -1,5 +1,32 @@
 package lab6.src;
-
-public class Square {
-
+/**
+ * Lab 6
+ * Class representing a square
+ * 
+ * @author Zandi Milligan
+ * @version 2019-02-21
+ *
+ */
+public class Square extends Rectangle
+{
+	/**
+	 * Constructor, references the rectangle constructor
+	 * @param id The shape's name identifier, as defined in Shape.java.
+	 * @param size The length of the sides of the square
+	 */
+	public Square(String id, double size)
+	{
+		super(id, size, size);
+	}
+	
+	/**
+	 * Gets the type of the shape.
+	 *
+	 * @return The string "Square".
+	 */
+	@Override
+	public String getShapeType()
+	{
+		return "Square";
+	}
 }
