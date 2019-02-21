d2ffc2553257144ae5f49c50acacd7e85057bc7d
Zandi Milligan <zmilligan@ou.edu>
2019-02-21 12:56:58 -0600

    created square and rectangle classes and todo list

---
+++ Rectangle.java
@@ -0,0 +1,5 @@
+package lab6.src;
+
+public class Rectangle {
+
+}
---
+++ Todo.java
@@ -0,0 +1,19 @@
+package lab6.src;
+public class Todo {/*
+Lab Section_________: 12
+Last Modified_______: 2019-02-14
+OU ID_______________: 112982309
+OU EMAIL____________: zmilligan@ou.edu
+
+Tasks:
+1. 
+2. 
+
+Estimated Time (minutes):
+1. 
+2. 
+
+Actual Time (minutes):
+1. 
+2. 
+*/}
