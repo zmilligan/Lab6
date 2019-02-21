2dfa9250e29dcc375bac58d4af30a8fb9848f7bf
Zandi Milligan <zmilligan@ou.edu>
2019-02-21 13:45:02 -0600

    updated

--- Todo.java
+++ Todo.java
@@ -9,14 +9,17 @@ Tasks:
 1. create square and rectangle classes skeleton
 2. implement Square method and constructor
 3. implement Rectangle method and constructor
+4. complete trapezoid constructor and getShapeType method
 
 Estimated Time (minutes):
 1. 6
 2. 14
 3. 14
+4. 5
 
 Actual Time (minutes):
 1. 8
 2. 16
-3.
+3. 17
+4. 7
 */}
